49f376bd1e6d1eb323905478190626796e488293
Martin <tsparksmartin@gmail.com>
2023-06-14 16:09:22 -0700

    Add setup to app

--- portal-QC-intertidal/app.R
+++ portal-QC-intertidal/app.R
@@ -1,5 +1,15 @@
+#### SETUP ####
 library(shiny)
+library(here)
+library(tidyverse)
+library(kcmarine)
+library(plotly)
+library(lubridate)
+library(htmlwidgets)
+source(here("src", "utility_functions.R"))
 
+# Suppress warnings (just for cleanliness)
+options(warn = -1)
 
 ui <- fluidPage(
   
